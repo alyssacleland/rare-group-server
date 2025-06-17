6be13ac7198b6bb0695624020aca3b09e12db44c
Damitry Dong <Damitry111@gmail.com>
2025-06-17 18:20:19 -0500

    setting page change to allow all

--- rare/settings.py
+++ rare/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_refz(sowm*jlti-a$08)p!!+d9@652-4$!87hnm+c9pb-m8b1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
