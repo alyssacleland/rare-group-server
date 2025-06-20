4d02d91f82d14bedc03b3ba5ea33c68a114a3cfc
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-06-20 17:47:34 -0500

    added depth to posttag for access to tag label on FE, fixed double create issue in post view (use .save() only, rm .create()

--- rareapi/serializers/postTag.py
+++ rareapi/serializers/postTag.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from ..models.postTag import PostTag
 
+
 class PostTagSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostTag
         fields = '__all__'
+        depth = 1
